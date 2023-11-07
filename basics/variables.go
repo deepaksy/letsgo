fe98c0d11b6dd08d44bfe89aa7620d8d2919854f
deepaksy <deepaksy28@gmail.com>
2023-11-07 11:01:44 +0530

    Added package for basics programs

---
+++ basics/variables.go
@@ -0,0 +1,12 @@
+package basics
+
+import "fmt"
+
+func Variables() {
+	/* Variables in go*/
+	fmt.Println("*******************" + "variables in go" + "*******************")
+	var numbers int8 = 12
+	var name string = "Deepak"
+	fmt.Println("Hello world")
+	fmt.Printf("%v\n%v", numbers, name)
+}
